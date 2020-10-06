a7f4a3972247f2bede2eea5844d31445c29d4577
ajg7 <ajgebara@gmail.com>
2020-10-06 11:26:48 -0500

    dependecies installed and server is set up

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const helmet = require("helmet"); // provides security defaults
+
+const server = express();
+
+server.use(helmet());
+server.use(express.json());
+
+server.get("/", (request, response) => {
+    response.status(200).json({Frankenstein: "It's alive!!!!"})
+})
+
+
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => console.log(`API running on port ${PORT}`));
